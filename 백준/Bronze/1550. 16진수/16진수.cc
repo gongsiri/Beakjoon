5f6997d1699f0cc6868a890d12fefc4b8c57eabc
공실 <80194907+gongsiri@users.noreply.github.com>
2024-07-05 17:35:11 +0900

    [Bronze II] Title: 16진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin >> hex >> x; // 16진수로 받아준다. (hex)
+	cout << dec << x; // 10진수로 출력해준다. (dec) (생략 가능)
+	return 0;
+}
+
+//2진법 bin
+//8진법 oct
+//10진법 dec
+//16진법 hex
